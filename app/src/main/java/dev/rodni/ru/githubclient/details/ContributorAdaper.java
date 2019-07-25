d448a3827c080e00400bdbdb886234202b029498
rodniw <47295698+rodniw@users.noreply.github.com>
2019-07-25 22:48:26 +0300

    contributor adapter(recycler) and contributor diff callback created. also some additions into ui section.

---
+++ app/src/main/java/dev/rodni/ru/githubclient/details/ContributorAdaper.java
@@ -0,0 +1,85 @@
+package dev.rodni.ru.githubclient.details;
+
+import android.support.annotation.NonNull;
+import android.support.v7.util.DiffUtil;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import dev.rodni.ru.githubclient.R;
+import dev.rodni.ru.githubclient.model.Contributor;
+
+class ContributorAdaper extends RecyclerView.Adapter<ContributorAdaper.ContributorViewHolder>{
+
+    private final List<Contributor> data = new ArrayList<>();
+
+    public ContributorAdaper() {
+        setHasStableIds(true);
+    }
+
+    @NonNull
+    @Override
+    public ContributorViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View itemView = LayoutInflater
+                .from(viewGroup.getContext())
+                .inflate(R.layout.view_user_list_item, viewGroup,false);
+        return new ContributorViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ContributorViewHolder contributorViewHolder, int i) {
+        contributorViewHolder.bind(data.get(i));
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return data.get(position).id();
+    }
+
+    void setData(List<Contributor> contributors) {
+        if (contributors != null) {
+            DiffUtil.DiffResult diffResult
+                    = DiffUtil.calculateDiff(new ContributorDiffCallback(data, contributors));
+            data.clear();
+            data.addAll(contributors);
+            diffResult.dispatchUpdatesTo(this);
+        } else {
+            data.clear();
+            notifyDataSetChanged();
+        }
+    }
+
+    static final class ContributorViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.tv_user_name)
+        TextView userNameText;
+        @BindView(R.id.iv_avatar)
+        ImageView avatarImageView;
+
+        public ContributorViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+
+        void bind(Contributor contributor) {
+            userNameText.setText(contributor.login());
+            Glide.with(avatarImageView.getContext())
+                    .load(contributor.avatarUrl())
+                    .into(avatarImageView);
+        }
+    }
+}
---
+++ app/src/main/java/dev/rodni/ru/githubclient/details/ContributorDiffCallback.java
@@ -0,0 +1,37 @@
+package dev.rodni.ru.githubclient.details;
+
+import android.support.v7.util.DiffUtil;
+
+import java.util.List;
+
+import dev.rodni.ru.githubclient.model.Contributor;
+
+public class ContributorDiffCallback extends DiffUtil.Callback {
+    private final List<Contributor> oldList;
+    private final List<Contributor> newList;
+
+    public ContributorDiffCallback(List<Contributor> oldList, List<Contributor> newList) {
+        this.oldList = oldList;
+        this.newList = newList;
+    }
+
+    @Override
+    public int getOldListSize() {
+        return oldList.size();
+    }
+
+    @Override
+    public int getNewListSize() {
+        return newList.size();
+    }
+
+    @Override
+    public boolean areItemsTheSame(int oldItemPosition, int newItemPosition) {
+        return oldList.get(oldItemPosition).id() == newList.get(newItemPosition).id();
+    }
+
+    @Override
+    public boolean areContentsTheSame(int oldItemPosition, int newItemPosition) {
+        return oldList.get(oldItemPosition).equals(newList.get(newItemPosition));
+    }
+}
