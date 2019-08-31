6d3184de520a4130eb500ac24c9384c650d297ea
rodniw <rodion.prosvirnin@bk.ru>
2019-08-31 19:45:19 +0300

    factory of the mock responses.

---
+++ app/src/debug/java/dev/rodni/ru/githubclient/networking/MockResponseFactory.java
@@ -0,0 +1,36 @@
+package dev.rodni.ru.githubclient.networking;
+
+import android.content.Context;
+
+import androidx.annotation.Nullable;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import okhttp3.Request;
+
+class MockResponseFactory {
+
+    private final Context context;
+    private final int startIndex;
+
+    //this class needs the context and a base url to inflate startIndex variable
+    @Inject
+    MockResponseFactory(Context context, @Named("base_url") String baseUrl) {
+        this.context = context;
+        startIndex = baseUrl.length();
+    }
+
+    //getting mock response(dividing the request with the "/") from the mock loader
+    @Nullable
+    String getMockResponse(Request request) {
+        String[] endpointParts = getEndpoint(request).split("/");
+        return MockResourceLoader.getResponseString(context, request.method(), endpointParts);
+    }
+
+    private String getEndpoint(Request request) {
+        String url = request.url().toString();
+        int queryParamStart = url.indexOf("?");
+        return queryParamStart == -1 ? url.substring(startIndex) : url.substring(startIndex, queryParamStart);
+    }
+}
